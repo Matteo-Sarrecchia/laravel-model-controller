a54d9545a94c8a1b746c73a7c4928bc9f6c9cc40
Matteo Sarrecchia <morrea@hotmail.it>
2023-07-12 16:58:35 +0200

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,3 @@
+<div class="container-fluid text-center py-3">
+    <h1>HEADER</h1>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.main-layout')
+
+@section('pageName')
+
+    home
+
+@endsection
+
+@section('content')
+
+    <main class="py-5 bg-primary">
+       <h1>hello world</h1>
+    </main>
+
+@endsection
